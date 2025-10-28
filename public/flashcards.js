33e5081298ea300235a0664a3c4342403de8ad66
wslider <wslider2000@gmail.com>
2025-10-28 13:00:20 -0400

    added form for creating new cards in flashcards.html and functions / event listeners in flashcards.js

--- public/flashcards.js
+++ public/flashcards.js
@@ -2,6 +2,8 @@ let flashcards = [];
 let currentIndex = 0;
 let isFlipped = false; 
 let isShuffled = false; 
+const engCardContent = document.getElementById('engCardContent'); 
+
 
 async function loadFlashcards () {
     try {
@@ -23,7 +25,65 @@ function displayCard() {
     document.getElementById('malayalam')=card.malayalam;
     document.getElementById('translit')=card.translit;
     document.getElementById('malExample')=card.malExample;
-    document.getElementById('malExampleTranslit')=card.malExampleTRanslit;
+    document.getElementById('malExampleTranslit')=card.malExampleTranslit;
     document.querySelectorAll('.cardContainer').forEach(c => c.classList.remove('flipped'));
     isFlipped = false; 
 }
+
+//flip on click - start with English side of card 
+
+engCardContent.addEventListener('click', ()=>{
+    if(!isFlipped){
+        engCardContent.classList.add('flipped');
+        isFlipped = true; 
+    }
+});
+
+//buttons 
+
+document.getElementById('nextButton').addEventListener('click',()=>{
+    currentIndex = (currentIndex + 1) % flashcards.length; 
+    displayCard(); 
+});
+
+document.getElementById('prevButton').addEventListener('click',()=>{
+    currentIndex = (currentIndex - 1 + flashcards.length) % flashcards.length;
+    displayCard(); 
+});
+
+document.getElementById('shuffleButton').addEventListener('click',()=>{
+    shuffled = !shuffled; 
+    flashcards = [...flashcards].sort(()=> Math.random()- 0.5); 
+    currentIndex = 0; 
+    displayCard(); 
+});
+
+document.getElementById('resetButton').addEventListener('click', ()=>{
+    loadFlashcards();
+    shuffled = false; 
+});
+
+//New Card Form - event listeners, input validation and functions 
+
+document.getElementById('newCardForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const newCard = {
+        id: Date.now(), //prints date created as id 
+        category: document.getElementById('newCategory').value, 
+        english: document.getElementById('newEnglish').value,
+        engExample: document.getElementById('newEngExample').value,
+        malayalam: document.getElementById('newMalayalam').value,
+        translit: document.getElementById('newTranslit').value,
+        malExample: document.getElementById('newMalExample').value,
+        malExampleTranslit: document.getElementById('newMalExampleTranslit').value,
+    };
+    await fetch('/api/flashcards', {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(newCard)
+    });
+    loadFlashcards();
+    e.target.reset();
+});
+
+
